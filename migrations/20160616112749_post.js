1106d91fad9b30cb7b7e5fb275c5d22ca47f0afc
Lucy Follansbee <lfollansbee@gmail.com>
2016-06-16 11:38:29 -0600

    setting up express app

---
+++ migrations/20160616112749_post.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('post', function(table){
+    table.increments()
+    table.string('title')
+    table.integer('user_id').references('user.id')
+    table.string('body')
+    table.string('img')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('post')
+};
