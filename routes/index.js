d9ae4a1783a5214e85114c2263016eb9d472354b
Lucy Follansbee <lfollansbee@gmail.com>
2016-06-20 10:35:55 -0600

    working index page and post of new blogs

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,30 @@ var router = express.Router();
 var pg = require('pg');
 var knex = require('../db/knex.js')
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  knex('post')
-  // .join('post', 'user.id', 'post.user_id')
-  .select()
+  knex('post').join('user', 'user_id', 'user.id')
   .then(function(posts){
-  res.render('index', {list: posts})
+    res.render('index', {list: posts})
   })
-});
-
-router.post('/write', function(req, res, next) {
-  knex('user').first().returning('id').insert({username: req.body.name})
-  .then(function(userid) {
-    return knex('post').insert({title: req.body.title, content: req.body.body, user_id: userid[0]});
-}).then(function() {
-    res.redirect('/');
-}).catch( function(error) {
-  });
 });
 
 router.get('/write', function(req, res, next){
     res.render('write')
 })
 
+router.post('/write', function(req, res, next) {
+  knex('user').first().returning('id').insert({name: req.body.name})
+  .then(function() {
+    return knex('post').insert(req.body);
+}).then(function() {
+    res.redirect('/');
+}).catch( function(error) {
+  });
+});
+
+
+// router.get('/:id/detail', function(req, res, next){
+//   knex('post')
+// })
 
 module.exports = router;
